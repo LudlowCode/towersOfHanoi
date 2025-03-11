2ce1d2a3b11c4d9475cede3e2f7ce2321fe330a2
LudlowCode <90901975+LudlowCode@users.noreply.github.com>
2025-03-11 08:58:31 +0000

    better car

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
-class Car 
+public class Car
 {
   //Attributes go here
-  string colour;
+  private string colour;
 
   //Constructor to make a new object of class Car..
   public Car(string aColour){
     colour = aColour;
   }
 
+  public string GetColour(){
+    return colour;
+  }
+
+  
+}
+public class Program{
   //Main runs automatically when the Program is run
+
   static void Main(string[] args)
   {
     //Calling the constructor of the Car class to make a new Car object
@@ -16,7 +24,7 @@ class Car
     Car myObj2 = new Car("Yellow");
 
     //Printing an attribute
-    Console.WriteLine(myObj.colour);
-    Console.WriteLine(myObj2.colour);
+    Console.WriteLine(myObj.GetColour());
+    Console.WriteLine(myObj2.GetColour());
   }
 }
